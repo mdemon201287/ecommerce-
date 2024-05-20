270753e2b1cf8b736507bab1bfbbf73671275039
mdemon201287 <mdemon201287@gmail.com>
2024-05-20 21:57:31 +0600

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import Routes
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <main className="py-3">
+        <Container>
+          {/* Use Routes instead of Route */}
+          <Routes>
+            <Route path='/' element={<HomeScreen />} />  
+            <Route path='/product/:id' element={<ProductScreen />} />
+          </Routes>
+        </Container>
+      </main>
+      <Footer />
+    </Router> 
+  );
+}
+
+export default App;
