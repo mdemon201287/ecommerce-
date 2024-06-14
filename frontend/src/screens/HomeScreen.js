1075527d7411fec6e58dd80db5604202bc03dbb3
mdemon201287 <mdemon201287@gmail.com>
2024-06-14 23:36:38 +0600

    updated

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,25 +1,28 @@
 import React, { useState, useEffect } from 'react'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
-import axios from 'axios'
+import Loader from '../components/Loader'  // Import Loader
+import { useDispatch, useSelector } from 'react-redux'
+import { listProducts } from '../actions/productActions'
+import Message from '../components/Message'
 
 function HomeScreen() {
-  const [products, setProducts] = useState([])
+  const dispatch = useDispatch()
+  const productList = useSelector(state => state.productList)
+  const { error, loading, products } = productList
+
+  const keyword = '' // Define keyword, adjust as needed for your use case
 
   useEffect(() => {
-    async function fetchProducts(){
-      const { data } = await axios.get('/api/products/')
-      setProducts(data)
-    }
-     
-
-    fetchProducts()
-
-  }, [])
+    dispatch(listProducts(keyword))
+  }, [dispatch, keyword])
 
   return (
     <div>
       <h1>Latest Products</h1>
+      {loading ? <Loader />
+                : error ? <Message variant='danger'>{error}</Message>
+                    :
       <Row>
         {products.map(product => (
             <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
@@ -27,6 +30,7 @@ function HomeScreen() {
             </Col>
         ))}
       </Row>
+      }
     </div>
   )
 }
